069ea77d12b63a06bb15b20cffe8e42b2d3a1ece
Gildo Chauze <gildochauze@gmail.com>
2024-08-12 08:13:22 +0200

    task 3 populate amenities combo

---
+++ web_dynamic/static/scripts/echo 'create City state_id="38df8860-482.sh
@@ -0,0 +1,1 @@
+echo 'create City state_id="38df8860-4820-4cb2-bfad-3d0c734ad209" name="San_Jose"' | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ./console.py
